fa58b3c2eb68dcd4c9e2371138c35a3570de5dd1
Qi <pangsc777@qq.com>
2026-03-01 00:10:29 +0800

    新添加图片识别，查询天气，联网搜索等功能  但是未写入workflow

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,21 +1,19 @@
 import os
+#加载.env文件
 from dotenv import load_dotenv
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
+load_dotenv()
 
-root_dir = os.path.dirname(os.path.dirname(current_dir))
+DEEPSEEK_API_KEY=os.getenv("DEEPSEEK_API_KEY")
+DEEPSEEK_MODEL_NAME=os.getenv("DEEPSEEK_MODEL_NAME","deepseek_chat")
+DEEPSEEK_BASE_URL=os.getenv("DEEPSEEK_BASE_URL")
 
-env_path = os.path.join(root_dir, ".env")
-
-load_dotenv(dotenv_path=env_path, override=True)
-
-DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
-DEEPSEEK_MODEL_NAME = os.getenv("DEEPSEEK_MODEL_NAME",)
-DEEPSEEK_BASE_URL = os.getenv("DEEPSEEK_BASE_URL")
-
-AMAP_WEATHER_KEY = os.getenv("AMAP_WEATHER_KEY", "a77d88e47400085190ed7d026002f905")
-
-TAVILY_API_KEY = os.getenv("TAVILY_API_KEY", "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")
+if not DEEPSEEK_API_KEY:
+    raise ValueError("未找到DEEPSEEK_API_KEY，请检查.env配置")
 
 
+# 高德天气 API Key
+AMAP_WEATHER_KEY = "a77d88e47400085190ed7d026002f905"
 
+# Tavily 搜索 API Key
+TAVILY_API_KEY = "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"
